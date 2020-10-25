a25a8cd73bb1f7183953a33d46fd0a1dc2b3d386
motoi21 <m250609@gmail.com>
2020-10-25 18:01:22 +0900

    バリデーションの記述の修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,21 +3,13 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  validates :nickname, presence: true
-  validates :last_name, presence: true
-  validates :first_name, presence: true
-  validates :kana_last_name, presence: true
-  validates :kana_first_name, presence: true
-  validates :birth_date, presence: true
-
-  with_options presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]+\z/ } do
-    validates :last_name
-    validates :first_name
-  end
-
-  with_options presence: true, format: { with: /\A[ァ-ン]+\z/ } do
-    validates :kana_last_name
-    validates :kana_first_name
+  with_options presence: true do       
+    validates :nickname
+    validates :last_name, format: { with: /\A[ぁ-んァ-ン一-龥]+\z/ }
+    validates :first_name, format: { with: /\A[ぁ-んァ-ン一-龥]+\z/ }
+    validates :kana_last_name, format: { with: /\A[ァ-ン]+\z/ }
+    validates :kana_first_name, format: { with: /\A[ァ-ン]+\z/ }
+    validates :birth_date
   end
 
   PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
