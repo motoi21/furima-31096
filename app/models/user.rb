f19841355ca940d6fa4e34cd4e6da58b6c24fdf7
motoi21 <m250609@gmail.com>
2020-10-25 19:48:11 +0900

    itemsテーブルの作成

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,9 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  
+  has_many :items
+         
   with_options presence: true do       
     validates :nickname
     validates :last_name, format: { with: /\A[ぁ-んァ-ン一-龥]+\z/ }
