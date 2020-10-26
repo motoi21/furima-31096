eb1883de21ef7778569e82eb1a32c054b558006b
motoi21 <m250609@gmail.com>
2020-10-26 19:15:52 +0900

    newアクションのビューファイルの作成

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -7,15 +7,16 @@ class ItemsController < ApplicationController
   end
 
   def create
-    @item = Item.new
+    @item = Item.new(item_params)
     if @item.save
       redirect_to root_path
     else render :new 
     end
+  end
 
   private
   def item_params
-    params.require(:item).permit(:name, :description, :category,:status, :shipping_charge, :prefecture, :days_to_ship, :price, :image).merge(user_id: current_user.id)
+    params.require(:item).permit(:name, :description, :category_id,:status_id, :shipping_charge_id, :prefecture_id, :days_to_ship_id, :price, :image).merge(user_id: current_user.id)
   end
   
 end
