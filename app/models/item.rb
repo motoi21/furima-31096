3172651e56513484106ad2d0ee89ee54bb80b416
motoi21 <m250609@gmail.com>
2020-10-26 12:05:00 +0900

    ActiveHashのアソシエーションの設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,3 +1,10 @@
 class Item < ApplicationRecord
   belongs_to :user
+
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to_active_hash :category
+  belongs_to_active_hash :status
+  belongs_to_active_hash :shipping_charge
+  belongs_to_active_hash :prefecture
+  belongs_to_active_hash :days_to_ship
 end
