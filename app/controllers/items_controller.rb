2e73d9ff4232e979049e2d0af4a262f5290b8485
motoi21 <m250609@gmail.com>
2020-10-26 16:50:29 +0900

    itemsモデルのストロングパラメーターの設定

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,10 @@
 class ItemsController < ApplicationController
   def index
   end
+
+  private
+  def item_params
+    params.require(:item).permit(:name, :description, :category,:status, :shipping_charge, :prefecture, :days_to_ship, :price, :image).merge(user_id: current_user.id)
+  end
+  
 end
