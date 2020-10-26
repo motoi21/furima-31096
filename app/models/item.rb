2e823b79daf0de0b300b1b7097c4cc7a3489ac49
motoi21 <m250609@gmail.com>
2020-10-26 16:40:26 +0900

    itensテーブルのバリデーションの設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -8,4 +8,23 @@ class Item < ApplicationRecord
   belongs_to_active_hash :shipping_charge
   belongs_to_active_hash :prefecture
   belongs_to_active_hash :days_to_ship
+
+  with_options presence: true do
+    validates :name
+    validates :description
+    validates :category
+    validates :status
+    validates :shipping_charge
+    validates :prefecture
+    validates :days_to_ship
+    validates :price, format: { with: /\A[0-9]+\z/ }, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999, message: "Price Out of setting range" }
+  end 
+  with_options numericality: { other_than: 1 } do
+    validates :category_id
+    validates :shipping_charge_id
+    validates :prefecture_id
+    validates :days_to_ship_id
+    validates :status_id
+  end
+  
 end
