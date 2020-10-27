c517c913f5932b5c190550cc2c7c37bce753e041
motoi21 <m250609@gmail.com>
2020-10-27 14:46:52 +0900

    itemモデルの単体テストを実施

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -1,5 +1,17 @@
 FactoryBot.define do
   factory :item do
-    
+    name { Faker::Lorem.sentence }
+    description { Faker::Lorem.sentences }
+    category_id { Faker::Number.between(from: 2, to: 11) }
+    status_id { Faker::Number.between(from: 2, to: 7) }
+    shipping_charge_id { Faker::Number.between(from: 2, to: 3) }
+    prefecture_id { Faker::Number.between(from: 2, to: 48) }
+    days_to_ship_id { Faker::Number.between(from: 2, to: 4) }
+    price { Faker::Number.between(from: 300, to: 9999999) }
+    association :user
+
+    after(:build) do |item|
+      item.image.attach(io: File.open('public/images/test_image.jpeg'),filename: 'test_image.jpeg')
+    end
   end
 end
