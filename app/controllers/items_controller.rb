7fb3c3e58c37d9d5139b393fdd8c3221866a1105
motoi21 <m250609@gmail.com>
2020-10-28 16:56:01 +0900

    move_to_indexの記述をbefore_actionにまとめる

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,8 +1,8 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
+  before_action :set_item, only: [:show, :edit, :update, :move_to_index]
   before_action :move_to_index, only: [:edit]
-  before_action :set_item, only: [:show, :edit, :update]
-
+  
   def index
     @items = Item.all.order("created_at DESC")
   end
@@ -40,8 +40,7 @@ class ItemsController < ApplicationController
   end
 
   def move_to_index
-    item = Item.find(params[:id])
-    unless user_signed_in? && current_user.id == item.user.id
+    unless user_signed_in? && current_user.id == @item.user.id
       redirect_to root_path
     end
   end
