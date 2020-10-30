277496466b5cc96c9e57c3847d5b0f6e91c5d7bb
motoi21 <m250609@gmail.com>
2020-10-30 13:20:24 +0900

    アソシエーションの設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,6 +1,8 @@
 class Item < ApplicationRecord
   belongs_to :user
+  has_one :purchase
   has_one_attached :image
+
 
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to_active_hash :category
--- app/models/purchase.rb
+++ app/models/purchase.rb
@@ -1,2 +1,9 @@
 class Purchase < ApplicationRecord
+  belongs_to :user
+  belongs_to :item
+  has_one :shipping
+
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to_active_hash :prefecture
+
 end
