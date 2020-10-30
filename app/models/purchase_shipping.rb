930bd8a209c912372bd24b366aaec980a6f03320
motoi21 <m250609@gmail.com>
2020-10-30 14:01:42 +0900

    バリデーションの設定

--- app/models/purchase_shipping.rb
+++ app/models/purchase_shipping.rb
@@ -1,4 +1,15 @@
 class PurchaseShipping
   include ActiveModel::Model
   attr_accessor :postal_code, :prefecture_id, :city, :address, :building, :tel
+
+  with_options presence: true do
+    validates :token
+    validates :postal_code, format: { with: /\A\d{3}[-]\d{4}\z/}
+    validates :city
+    validates :address
+    validates :tel, numericality: { only_integer: true }, length: { maximum: 11 }
+  end
+
+  validates :prefecture_id, numericality: { other_than: 1, message: 'Select' }
+
 end
