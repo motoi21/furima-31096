9583f80df1f94402b9cd3d053e5126f3d8bd4273
motoi21 <m250609@gmail.com>
2020-10-30 14:46:46 +0900

    new,createアクションの定義

--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -1,4 +1,17 @@
 class PurchasesController < ApplicationController
+  def new
+    @purchase_shipping = PurchaseShipping.new
+  end
+
+  def create
+    @purchase_shipping = PurchaseShipping.new(purchase_params)
+    if @purchase_shipping.valid?
+      @purchase_shipping.save
+      redirect_to root_path
+    else 
+      render :new
+    end
+  end
   private
   def purchase_params
     params.require(:purchase_shipping).permit(:postal_code, :prefecture_id, :city, :address, :building, :tel).merge(user_id: current_user.id, item_id: params[:item_id]) 
--- app/models/purchase_shipping.rb
+++ app/models/purchase_shipping.rb
@@ -3,7 +3,6 @@ class PurchaseShipping
   attr_accessor :postal_code, :prefecture_id, :city, :address, :building, :tel
 
   with_options presence: true do
-    validates :token
     validates :postal_code, format: { with: /\A\d{3}[-]\d{4}\z/}
     validates :city
     validates :address
