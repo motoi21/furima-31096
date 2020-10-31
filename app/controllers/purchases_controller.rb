8ffaa0f591ebbaee618b5774a71b222877d132cf
motoi21 <m250609@gmail.com>
2020-10-31 18:09:34 +0900

    ログイン状態の有無、出品者かどうかで商品購入ページへ行けないように記述

--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -1,4 +1,9 @@
 class PurchasesController < ApplicationController
+  before_action :authenticate_user!
+  before_action :set_item, only:[:index, :create]
+  before_action :move_to_index, only:[:index, :create]
+
+
   def index
     @purchase_shipping = PurchaseShipping.new
     @item = Item.find(params[:item_id])
@@ -24,4 +29,18 @@ class PurchasesController < ApplicationController
   def purchase_params
     params.require(:purchase_shipping).permit(:postal_code, :prefecture_id, :city, :address, :building, :tel).merge(user_id: current_user.id, item_id: params[:item_id],token: params[:token])
   end
+
+  private
+  def set_item
+    @item = Item.find(params[:item_id])
+  end
+
+  def move_to_index
+    if user_signed_in? && current_user.id == @item.user.id
+      redirect_to root_path
+    elsif Purchase.existss?(item_id: @item.id)
+      redirect_to root_path
+    end
+  end
+
 end
